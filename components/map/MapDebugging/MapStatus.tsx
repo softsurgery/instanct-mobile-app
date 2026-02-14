7ff4e03812d812f02f086da253356d6ec24a182e
softsurgery <109615722+softsurgery@users.noreply.github.com>
2026-02-15 00:36:52 +0100

    feat(map): add MapDebugDialog components for enhanced debugging information

--- components/map/MapDebugging/MapStatus.tsx
+++ components/map/MapDebugging/MapStatus.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { useMapStore } from "@/stores/useMapStore";
 import { View } from "react-native";
-import { Text } from "../ui/text";
+import { Text } from "../../ui/text";
 
 interface MapStatusProps {
   className?: string;
