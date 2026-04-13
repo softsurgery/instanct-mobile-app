741ded2f2150b0635761b3a58138fb83b212ad3a
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-04-13 23:19:21 +0000

    fix(form-builder): handle potential null values for hours and minutes for TimePicker component

--- components/shared/form-builder/TimePicker.tsx
+++ components/shared/form-builder/TimePicker.tsx
@@ -58,8 +58,8 @@ const PERIODS = [
 ];
 
 function formatTime(date: Date): string {
-  let hours = date.getHours();
-  const minutes = date.getMinutes();
+  let hours = date?.getHours();
+  const minutes = date?.getMinutes();
   const period = hours >= 12 ? "PM" : "AM";
   hours = hours % 12 || 12;
   return `${hours}:${String(minutes).padStart(2, "0")} ${period}`;
