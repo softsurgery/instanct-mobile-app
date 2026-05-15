03f0447bcfcb8521636fd2c8d997af9523cc43dc
softsurgery <housssemeddine.chibouni@fsb.ucar.tn>
2026-05-15 11:27:31 +0000

    fix(user): add missing user fetching endpoints

--- api/user.ts
+++ api/user.ts
@@ -1,4 +1,5 @@
 import {
+  Paginated,
   QueryParams,
   ResponseConfigurationNamespaceDto,
   ResponseUserDto,
@@ -7,6 +8,7 @@ import {
   UpdateUserMapConfigurationDto,
 } from "@/types";
 import axios from "./axios";
+import { ResponseUserBookmarkDto } from "@/types/bookmark";
 
 const findCurrent = async (): Promise<ResponseUserDto> => {
   const response = await axios.get<ResponseUserDto>(`/current-user`);
@@ -20,6 +22,32 @@ const findById = async (
   const response = await axios.get<ResponseUserDto>(`/admin/user/${id}`, {
     params: query,
   });
+  return response.data;
+};
+
+const findByUsername = async (
+  username: string,
+  query?: Pick<QueryParams, "join">,
+): Promise<ResponseUserDto> => {
+  const response = await axios.get<ResponseUserDto>(
+    `/admin/user/username/${username}`,
+    {
+      params: query,
+    },
+  );
+  return response.data;
+};
+
+const findByEmail = async (
+  email: string,
+  query?: Pick<QueryParams, "join">,
+): Promise<ResponseUserDto> => {
+  const response = await axios.get<ResponseUserDto>(
+    `/admin/user/email/${email}`,
+    {
+      params: query,
+    },
+  );
   return response.data;
 };
 
@@ -81,14 +109,46 @@ const updateMapConfiguration = async (
   return response.data;
 };
 
+const findPaginatedBookmarks = async ({
+  page = "1",
+  limit = "5",
+  sort,
+  search = "",
+  filter = "",
+  join = "",
+}: QueryParams): Promise<Paginated<ResponseUserBookmarkDto>> => {
+  const params: { [key: string]: any } = {
+    page,
+    limit,
+    sort,
+  };
+
+  if (search) params.search = search;
+  if (filter) params.filter = filter;
+  if (join) params.join = join;
+
+  const response = await axios.get<Paginated<ResponseUserBookmarkDto>>(
+    `/current-user/bookmarks/list`,
+
+    {
+      params,
+    },
+  );
+
+  return response.data;
+};
+
 export const user = {
   findAll,
   findCurrent,
   findById,
+  findByEmail,
+  findByUsername,
   updateCurrent,
   updateIndustries,
   updateCover,
   getIndustries,
   getCurrentMapConfiguration,
   updateMapConfiguration,
+  findPaginatedBookmarks,
 };
