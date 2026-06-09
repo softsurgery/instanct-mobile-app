c6fd0300f9a1257c2afd09ec31fedc53c27f1f0d
softsurgery <housssemeddine.chibouni@fsb.ucar.tn>
2026-06-09 23:39:00 +0000

    chore: add extra margin in sign in for smaller devices

--- components/auth/SigninLayout.tsx
+++ components/auth/SigninLayout.tsx
@@ -111,7 +111,7 @@ export const SigninLayout = ({ className }: SigninProps) => {
               <SSOButtons isSignInPending={isSignInPending} />
             </View>
 
-            <View className="flex flex-row gap-1 items-center justify-center">
+            <View className="flex flex-row gap-1 items-center justify-center mb-10">
               <Text className="text-base">Don&apos;t have an account?</Text>
               <Text
                 className="text-base font-bold"
