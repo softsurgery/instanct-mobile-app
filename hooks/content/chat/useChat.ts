cc09f48e0e65abe5f908da59d2ee46bf3752782d
softsurgery <housssemeddine.chibouni@fsb.ucar.tn>
2026-07-09 09:46:34 +0000

    ref(chat): migrate all chat-related custom hooks from .tsx to .ts extension

--- hooks/content/chat/useChat.ts
+++ hooks/content/chat/useChat.ts
@@ -147,13 +147,13 @@ export const useChat = (
                   ? updated.lastMessage.content
                   : updated.lastMessage.variant === MessageVariant.IMAGE
                     ? "📷 Image"
-                  : updated.lastMessage.variant === MessageVariant.VIDEO
-                    ? "🎥 Video"
-                    : updated.lastMessage.variant === MessageVariant.FILE
-                      ? "📎 File"
-                      : updated.lastMessage.variant === MessageVariant.EMOJI
-                        ? updated.lastMessage.content
-                        : "",
+                    : updated.lastMessage.variant === MessageVariant.VIDEO
+                      ? "🎥 Video"
+                      : updated.lastMessage.variant === MessageVariant.FILE
+                        ? "📎 File"
+                        : updated.lastMessage.variant === MessageVariant.EMOJI
+                          ? updated.lastMessage.content
+                          : "",
               sound: true,
             },
             trigger: null,
