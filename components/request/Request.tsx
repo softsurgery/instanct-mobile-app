9f709f592bb870bdfa85f93dc13beceeeb00cf79
softsurgery <housssemeddine.chibouni@fsb.ucar.tn>
2026-08-02 13:37:20 +0000

    fix(activities): correct conditional rendering logic for request skeleton state

--- components/request/Request.tsx
+++ components/request/Request.tsx
@@ -167,7 +167,7 @@ export const Request = ({
         ]}
       />
       <View className="flex-1 bg-background p-4">
-        {!isPending ? (
+        {isPending ? (
           <RequestSkeleton hasLocation={hasLocation} />
         ) : (
           <View className="flex flex-col gap-4">
