f4688257964d9ec93ff423afe6fc88d38ea82d42
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-08-06 15:34:19 +0000

    fix: repair checkbox selection

--- components/shared/form-builder/FieldBuilder.tsx
+++ components/shared/form-builder/FieldBuilder.tsx
@@ -151,10 +151,10 @@ export const FieldBuilder = ({ field }: FieldBuilderProps) => {
       );
     case "checkbox":
       return (
-        <View className="flex-row items-center gap-2 -mt-2">
+        <View className="flex-row items-center gap-2">
           <Checkbox
             {...field?.props}
-            disabled={!editable === false}
+            disabled={!editable}
             checked={field?.props?.checked}
             onCheckedChange={(checked) => {
               field?.props?.onCheckedChange?.(checked);
@@ -163,7 +163,7 @@ export const FieldBuilder = ({ field }: FieldBuilderProps) => {
               root: cn(field?.className, field?.error && "border-red-500"),
             }}
           />
-          <Text className="text-sm">{field.description}</Text>
+          <Text className="text-sm pr-8">{field.description}</Text>
         </View>
       );
 
